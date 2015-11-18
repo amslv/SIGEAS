154c33535797fe4a1415f52466c3be1214bcca58
Joaquim Maia <joaquim.cmh@gmail.com>
2015-11-18 15:01:18 -0300

    Menu lateral finalizado

--- 02-SISTEMA/03-IMPLEMENTACAO/SIGEAS/WebContent/resources/javascript/script.js
+++ 02-SISTEMA/03-IMPLEMENTACAO/SIGEAS/WebContent/resources/javascript/script.js
@@ -2,7 +2,8 @@
 	$(document).ready(function() {
 
 		$('#cssmenu li.active').addClass('open').children('ul').show();
-		$('#cssmenu li.has-sub>a').on('click', function() {
+
+		$('#cssmenu li.activities>a').on('click', function() {
 			$(this).removeAttr('href');
 			var element = $(this).parent('li');
 
@@ -19,6 +20,116 @@
 				element.siblings('li').find('ul').slideUp(200);
 			}
 		});
+		
+		$('#cssmenu li.programs>a').on('click', function() {
+			$(this).removeAttr('href');
+			var element = $(this).parent('li');
+
+			if (element.hasClass('open')) {
+				element.removeClass('open');
+				element.find('li').removeClass('open');
+				element.find('ul').slideUp(200);
+			} else {
+				element.addClass('open');
+				element.children('ul').slideDown(200);
+				element.siblings('li').children('ul').slideUp(200);
+				element.siblings('li').removeClass('open');
+				element.siblings('li').find('li').removeClass('open');
+				element.siblings('li').find('ul').slideUp(200);
+			}
+		});
+		
+		$('#cssmenu li.concessions>a').on('click', function() {
+			$(this).removeAttr('href');
+			var element = $(this).parent('li');
+
+			if (element.hasClass('open')) {
+				element.removeClass('open');
+				element.find('li').removeClass('open');
+				element.find('ul').slideUp(200);
+			} else {
+				element.addClass('open');
+				element.children('ul').slideDown(200);
+				element.siblings('li').children('ul').slideUp(200);
+				element.siblings('li').removeClass('open');
+				element.siblings('li').find('li').removeClass('open');
+				element.siblings('li').find('ul').slideUp(200);
+			}
+		});
+
+		$('#cssmenu li.users>a').on('click', function() {
+			$(this).removeAttr('href');
+			var element = $(this).parent('li');
+
+			if (element.hasClass('open')) {
+				element.removeClass('open');
+				element.find('li').removeClass('open');
+				element.find('ul').slideUp(200);
+			} else {
+				element.addClass('open');
+				element.children('ul').slideDown(200);
+				element.siblings('li').children('ul').slideUp(200);
+				element.siblings('li').removeClass('open');
+				element.siblings('li').find('li').removeClass('open');
+				element.siblings('li').find('ul').slideUp(200);
+			}
+		});
+		
+		$('#cssmenu li.reports>a').on('click', function() {
+			$(this).removeAttr('href');
+			var element = $(this).parent('li');
+
+			if (element.hasClass('open')) {
+				element.removeClass('open');
+				element.find('li').removeClass('open');
+				element.find('ul').slideUp(200);
+			} else {
+				element.addClass('open');
+				element.children('ul').slideDown(200);
+				element.siblings('li').children('ul').slideUp(200);
+				element.siblings('li').removeClass('open');
+				element.siblings('li').find('li').removeClass('open');
+				element.siblings('li').find('ul').slideUp(200);
+			}
+		});
+
+		$('#cssmenu li.about>a').on('click', function() {
+			$(this).removeAttr('href');
+			var element = $(this).parent('li');
+
+			if (element.hasClass('open')) {
+				element.removeClass('open');
+				element.find('li').removeClass('open');
+				element.find('ul').slideUp(200);
+			} else {
+				element.addClass('open');
+				element.children('ul').slideDown(200);
+				element.siblings('li').children('ul').slideUp(200);
+				element.siblings('li').removeClass('open');
+				element.siblings('li').find('li').removeClass('open');
+				element.siblings('li').find('ul').slideUp(200);
+			}
+		});
+
+		$('#cssmenu li.help>a').on('click', function() {
+			$(this).removeAttr('href');
+			var element = $(this).parent('li');
+
+			if (element.hasClass('open')) {
+				element.removeClass('open');
+				element.find('li').removeClass('open');
+				element.find('ul').slideUp(200);
+			} else {
+				element.addClass('open');
+				element.children('ul').slideDown(200);
+				element.siblings('li').children('ul').slideUp(200);
+				element.siblings('li').removeClass('open');
+				element.siblings('li').find('li').removeClass('open');
+				element.siblings('li').find('ul').slideUp(200);
+			}
+		});
+
+		
 
 	});
 })
